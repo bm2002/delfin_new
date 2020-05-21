913c909c7987e33bdb00439001acac51d32f3978
max_home <bm_2002@bk.ru>
2020-05-21 20:10:04 +0300

    21052020-2009

---
+++ Controllers/json2Controller.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Delfin.Shared.Data;
+using Newtonsoft.Json;
+using qiwi.Models;
+
+namespace qiwi.Controllers
+{
+    public class json2Controller : Controller
+    {
+        MTPricesDataContext db = new MTPricesDataContext(ConfigurationManager.ConnectionStrings["apiDatabase"].ToString());
+        logContext log = new logContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult Get_Dlf_Active_Tour()
+        {
+            var d = DateTime.Now;
+
+            var t = (from tt in db.TP_Tours
+                     join tl in db.TP_Lists on tt.TO_Key equals tl.TI_TOKey
+                     join td in db.TP_TurDates on tt.TO_Key equals td.TD_TOKey
+                     where td.TD_Date >= DateTime.Now
+                     select tt.TO_TRKey).Distinct().ToList();
+
+            log.RequestDumps2.Add(new RequestDump2
+            {
+                ProviderId = 201,
+                Url = Request.Url.AbsoluteUri,
+                Success = true,
+                Duration = DateTime.Now - d,
+                Response = JsonConvert.SerializeObject(t),
+                ClientIP = Request.ServerVariables["HTTP_X_FORWARDED_FOR"] ?? Request.UserHostAddress,
+                Created = DateTime.Now,
+                Label = "Get_Dlf_Active_Tour"
+            });
+            log.SaveChanges();
+
+            return Json(t, JsonRequestBehavior.AllowGet); 
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Models/logContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace qiwi.Models
+{
+    public class logContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(ConfigurationManager.ConnectionStrings["Logs"].ConnectionString);
+                //optionsBuilder.UseSqlServer("Data Source=192.168.0.16;Initial Catalog=Logs;Integrated Security=False;User ID=sa;Password=23#swde23#;Connect Timeout=15;Encrypt=False;TrustServerCertificate=False;");
+            }
+        }
+
+        public DbSet<RequestDump2> RequestDumps2 { get; set; } 
+    }
+
+    public class RequestDump2
+    {
+        public int Id { get; set; }
+        public DateTime Created { get; set; }
+        public string ClientIP { get; set; }
+        public string Url { get; set; }
+        public string Label { get; set; }
+        public string Request { get; set; }
+        public string Response { get; set; }
+        [MaxLength(10)]
+        public string OrderId { get; set; }
+        public int ErrorCode { get; set; }
+        public string ErrorMessage { get; set; }
+        public string Comment { get; set; }
+        public int ProviderId { get; set; }
+        public bool Success { get; set; }
+        public TimeSpan Duration { get; set; }
+    }
+}
